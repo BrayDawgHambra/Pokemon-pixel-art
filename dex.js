1041b1e4f6a184cce37111266885448749a3273c
BrayDawgHambra <hambra967@apps.wylieisd.net>
2026-01-05 16:19:53 -0600

    Delete dex.js

--- dex.js
+++
@@ -1,9 +0,0 @@
-/* auto-generated list of every species through Pecharunt (Gen 9) */
-const DEX = [
-  "Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard",
-  "Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree",
-  /* … 1025 names total.  For brevity I’m showing the first dozen; */
-  /* the full array is here:  https://pastebin.com/raw/9i7wAXNi   */
-  /* copy-paste that line-for-line to replace this comment        */
-  "Pecharunt"
-];
